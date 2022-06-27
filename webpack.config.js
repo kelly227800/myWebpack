18034fd7b1610078ca0f33b180f4f3d4aba93225
kelly227800 <2529303733@qq.com>
2022-06-27 22:37:08 +0800

    配置了入口出口、插件

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js", // 入口
+    output: { 
+        path: path.resolve(__dirname, "dist"), // 出口路径 绝对路径
+        filename: "main.js" // 出口文件名
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // 告诉webpack使用插件时, 以我们自己的html文件作为模板去生成dist/html文件
+            filename: 'index.html' // 生成文件的名称
+        }),
+        new CleanWebpackPlugin(),
+    ]
+}
